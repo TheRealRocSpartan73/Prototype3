8c193041b0c287014d58400fcabb04c18d6196ea
TheRealRocSpartan73 <146456843+TheRealRocSpartan73@users.noreply.github.com>
2023-10-14 20:29:47 +0100

    Section 3.1.4 Sorted

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRB;
+    private int jumpForce = 10;
     // Start is called before the first frame update
     void Start()
     {
+        playerRB = GetComponent<Rigidbody>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Check for space bar and make player jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); //Burst the impulse force.
+        }
     }
 }
