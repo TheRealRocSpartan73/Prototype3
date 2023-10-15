83e1858c57265ce043528de1d58e33b0c00f56c0
TheRealRocSpartan73 <146456843+TheRealRocSpartan73@users.noreply.github.com>
2023-10-15 17:56:12 +0100

    3.4 Complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
@@ -6,18 +7,26 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRB;
+    private AudioSource playerAudio;
     public int jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver = false;
     private Animator playerAnimation;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dustParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource cameraAudio;
     // Start is called before the first frame update
     void Start()
     {
         //Once player class is created, obtain references to the player object 
         //Rigidbody and Animator components
-        playerRB = GetComponent<Rigidbody>();
+        playerRB = this.GetComponent<Rigidbody>();
         playerAnimation=this.GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        cameraAudio = GameObject.Find("Main Camera").GetComponent<AudioSource>();
         
         Physics.gravity *= gravityModifier;
     }
@@ -28,10 +37,14 @@ public class PlayerController : MonoBehaviour
         //Check for space bar and make player jump
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true && !gameOver)
         {
+            
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); //Burst the impulse force.
             isOnGround = false;
             //Spacebar triggers the "Running Jump" animation
             playerAnimation.SetTrigger("Jump_trig");
+            //Turn off dirt whilst jumping
+            dustParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f); //Play jumpSound
         }
     }
 
@@ -41,13 +54,20 @@ public class PlayerController : MonoBehaviour
         if(collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
-        }else if (collision.gameObject.CompareTag("Obstacle"))
+            dustParticle.Play();
+        }else if (collision.gameObject.CompareTag("Obstacle")) //Game Over State
         {
             Debug.Log("***** GAME OVER MAN ******");
             gameOver = true;
-            //play the death animation
+            //Play Crash sound
+            
+            //play the death animation and start explosion particle effect
             playerAnimation.SetInteger("DeathType_int", 1);
             playerAnimation.SetBool("Death_b", true);
+            explosionParticle.Play();
+            dustParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f); //Play crashSound
+            cameraAudio.Stop();//Stop the main tune
         }
 
     }
